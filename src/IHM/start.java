59ae6d3f3f878186b321f65af447cfbc98ba848f
20181591 <20181591@C22-19.etudiants.campus.villejuif>
2018-12-17 11:43:13 +0100

    FENETRE MERE

--- src/IHM/start.java
+++ src/IHM/start.java
@@ -17,15 +17,8 @@ public class start {
 	  * @param args the command line arguments
 	  */
 	 public static void main(String[] args) {
-		  // TODO code application logic here
-		  JFrame windows = new JFrame();
-		  //PanelAdministration panel = new PanelAdministration();
-		  PanelAffichage panel = new PanelAffichage();
-		  windows.add(panel);
-		  windows.setVisible(true);
-		  windows.setSize(300, 300);
-		  windows.setLocation(500, 300);
-		  windows.pack();
+		  FenetreMere windows = new FenetreMere();
+		
 	 }
 	 
 }
